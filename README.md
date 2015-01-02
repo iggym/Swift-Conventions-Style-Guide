53778c4998eb73fa8545e90ad1b00c97404ad7e8
Iggy Mwangi <iggy.mw@gmail.com>
2015-01-01 22:41:08 -0600

    Added Swift Style playground
    Swift Style playground

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Swift Style Guide.
+# Swift Conventions and Style Guide.
 
 The goals for this Swift Style Guide are to encourage readable and concise swift code.
+
 
---
+++ Swift-Conventions-Style-Guide-iOS.playground/section-1.swift
@@ -0,0 +1,4 @@
+// Swift Conventions and Style Guide. - on readable and concise swift code.
+//MARK: - Swift Style playground
+import UIKit
+var str = "Swift Conventions and Style Guide"
