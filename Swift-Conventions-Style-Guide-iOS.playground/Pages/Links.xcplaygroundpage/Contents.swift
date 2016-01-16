cc1a4b494f17227d19b762211a31577c95e7fbba
Iggy Mwangi <iggy.mw@gmail.com>
2016-01-16 10:55:25 -0600

    added links

---
+++ Swift-Conventions-Style-Guide-iOS.playground/Pages/Links.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+// Playground - noun: a place where people can play
+//See TODOs for code conventions or the preffered way
+//see https://github.com/raywenderlich/swift-style-guide
+//see https://github.com/github/swift-style-guide
