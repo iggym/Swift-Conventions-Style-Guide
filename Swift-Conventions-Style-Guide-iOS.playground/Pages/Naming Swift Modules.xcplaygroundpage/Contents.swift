591d45d8f259a41092e197c5b6f6a2060865beb7
Iggy Mwangi <iggy.mw@gmail.com>
2016-01-17 04:38:23 -0600

    added Namespacing

--- Swift-Conventions-Style-Guide-iOS.playground/Pages/Naming Swift Modules.xcplaygroundpage/Contents.swift
+++ Swift-Conventions-Style-Guide-iOS.playground/Pages/Naming Swift Modules.xcplaygroundpage/Contents.swift
@@ -2,6 +2,25 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//////////////////////////////////////////////////////////////////////////////--
+
+//MARK: - Swift Module names are Pascal Case
+
+/*Namespacing is implicit in swift,
+All classes (etc) are implicitly scoped by the module (Xcode target) they are in.
+no class prefixes needed
+
+Namespaces are not per-file;
+they're per-target (based on the "Product Module Name" build setting).
+*/
+// TODO: Use Pascal Case for modules
+//import FrameworkA
+//import FrameworkB
+
+//FrameworkA.foo()
+
+//////////////////////////////////////////////////////////////////////////////--
+
+
 
 //: [Next](@next)
